551d29ca382d0d8c3edca844f6cb9d03ac81008a
Robooto <Robooto@DESKTOP-A5DPVKQ>
2016-07-25 19:23:31 -0700

    Add project files.

---
+++ FileConvertService/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Topshelf;
+
+namespace FileConvertService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HostFactory.Run(serviceConfig =>
+            {
+                serviceConfig.UseNLog();
+
+                serviceConfig.Service<ConverterService>(serviceInstance =>
+                {
+                    serviceInstance.ConstructUsing(() => new ConverterService());
+
+                    serviceInstance.WhenStarted(execute => execute.Start());
+
+                    serviceInstance.WhenStopped(execute => execute.Stop());
+
+                    serviceInstance.WhenPaused(execute => execute.Pause());
+
+                    serviceInstance.WhenContinued(execute => execute.Continue());
+
+                    serviceInstance.WhenCustomCommandReceived((execute, hostControl, commandNumber) => execute.CustomCommand(commandNumber));
+                });
+
+                serviceConfig.EnableServiceRecovery(recoveryOption =>
+                {
+                    recoveryOption.RestartService(1);
+                    recoveryOption.RestartService(4);
+                    recoveryOption.RunProgram(5, @"d:\someprogram.exe");
+                });
+
+                serviceConfig.EnablePauseAndContinue();
+
+                serviceConfig.SetServiceName("AwesomeFileConverter");
+                serviceConfig.SetDisplayName("Awesome File Converter");
+                serviceConfig.SetDescription("A demo service");
+
+                serviceConfig.StartAutomatically();
+            });
+        }
+    }
+}
